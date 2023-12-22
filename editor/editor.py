769c3cf56e06524b374a8b4fa9ec3c92116e8141
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-12-21 22:42:06 -0300

    Aplicação guardanapo de editor de texto compartilhado em websocket python e javascript criada

---
+++ editor/editor.py
@@ -0,0 +1,25 @@
+import asyncio
+import websockets
+
+# Lista para armazenar todos os clientes conectados
+clients = set()
+
+async def server(websocket, path):
+    # Adiciona o novo cliente à lista
+    clients.add(websocket)
+    
+    try:
+        async for message in websocket:
+            # Envie a mensagem recebida para todos os clientes conectados
+            for client in clients:
+                await client.send(message)
+    finally:
+        # Remove o cliente da lista quando desconectado
+        clients.remove(websocket)
+
+# Inicia o servidor WebSocket
+start_server = websockets.serve(server, "localhost", 8765)
+
+# Executa o loop de eventos
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
---
+++ editor/leiame.md
@@ -0,0 +1,29 @@
+# Websocket Guarnapado: Editor de texto compartilhado
+## Preparando o ambiente
+Crie um ambiente virtual
+```
+python -m venv venv
+venv\Scripts\activate
+```
+Instale as bibliotecas necessárias
+```
+python -m pip install --upgrade pip
+pip install -r req.txt
+```
+## Executando a aplicação
+
+Ative o ambiente virtual e execute
+```
+python editor.py
+```
+Abra no navegador o arquivo:
+
+```
+editor.html
+```
+
+## Encerramento do ambiente
+```
+deactivate
+rm venv
+```
