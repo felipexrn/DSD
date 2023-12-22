e86e3612e7cee27e928b1ee4585e4c6b5d995f54
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-12-22 08:00:20 -0300

    adaptação à máquina do prof

--- editor/editor.py
+++ editor/editor.py
@@ -18,7 +18,7 @@ async def server(websocket, path):
         clients.remove(websocket)
 
 # Inicia o servidor WebSocket
-start_server = websockets.serve(server, "localhost", 8765)
+start_server = websockets.serve(server, "10.25.2.30", 8765)
 
 # Executa o loop de eventos
 asyncio.get_event_loop().run_until_complete(start_server)
